fd22f9cec933030e1d880bdc25d1a7c01c30e413
erikleemet <erik.leemet@gmail.com>
2022-12-06 08:56:35 +0200

    new

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+        <header>
+            <ul>
+                <li><a href="/Dashboard">Dashboard</a></li>
+                <li><a href="/Procurements">Procurements</a></li>
+                <li><a href="/Resources">Resources</a></li>
+                <li><a href="/Projects">Projects</a></li>
+            </ul>
+        </header>
+    )
+}
---
+++ src/pages/Procurements.js
@@ -0,0 +1,5 @@
+export default function Procurements() {
+    return (
+        <h1>Procurements</h1>
+    )
+}
