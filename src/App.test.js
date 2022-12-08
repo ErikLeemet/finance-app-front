9c86a1b8996ab2bbd5fbf1ed20c966c898a6d0cb
erikleemet <erik.leemet@gmail.com>
2022-12-08 09:51:48 +0200

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -5,19 +5,19 @@ import React from 'react'
 import '@testing-library/jest-dom'
 import BrowserRouter from 'react-router-dom'
 
-test('full app rendering/navigating', async () => {
+test('Navigation links are displayed', () => {
   render(<App />, {wrapper: BrowserRouter})
   const user = userEvent.setup()
     // verify page content for expected route after navigating
-    await user.click(screen.getByText(/Dashboard/i));
+    user.click(screen.getByText(/Dashboard/i));
     expect(screen.getByText(/Dashboard/i)).toBeInTheDocument();
 
-    await user.click(screen.getByText(/Procurements/i))
+    user.click(screen.getByText(/Procurements/i))
     expect(screen.getByText(/Procurements/i)).toBeInTheDocument()
 
-    await user.click(screen.getByText(/Resources/i))
+    user.click(screen.getByText(/Resources/i))
     expect(screen.getByText(/Resources/i)).toBeInTheDocument()
 
-    await user.click(screen.getByText(/Projects/i))
+    user.click(screen.getByText(/Projects/i))
     expect(screen.getByText(/Projects/i)).toBeInTheDocument()
   })
