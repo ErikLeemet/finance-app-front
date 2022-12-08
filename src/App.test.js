4514d9d7c804e616fff8f710fcdbeaea66e80d31
erikleemet <erik.leemet@gmail.com>
2022-12-08 08:58:59 +0200

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
+import React from 'react'
+import '@testing-library/jest-dom'
+import BrowserRouter from 'react-router-dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('full app rendering/navigating', async () => {
+  render(<App />, {wrapper: BrowserRouter})
+  const user = userEvent.setup()
+    // verify page content for expected route after navigating
+    await user.click(screen.getByText(/Dashboard/i));
+    expect(screen.getByText(/Dashboard/i)).toBeInTheDocument();
+
+    await user.click(screen.getByText(/Procurements/i))
+    expect(screen.getByText(/Procurements/i)).toBeInTheDocument()
+
+    await user.click(screen.getByText(/Resources/i))
+    expect(screen.getByText(/Resources/i)).toBeInTheDocument()
+
+    await user.click(screen.getByText(/Projects/i))
+    expect(screen.getByText(/Projects/i)).toBeInTheDocument()
+  })
