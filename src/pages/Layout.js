e494da6e445f63fafea0e01c39f6a75cdc75babc
erikleemet <erik.leemet@gmail.com>
2022-12-09 09:58:29 +0200

    styles

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,9 +1,10 @@
 import { Outlet, Link } from "react-router-dom";
+import '../layout.css';
 
 const Layout = () => {
   return (
     <>
-      <nav>
+      <nav className="navbar">
         <ul>
           <li>
             <Link to="/Dashboard">Dashboard</Link>
