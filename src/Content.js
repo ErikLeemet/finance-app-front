cbe65f94424b95851c6f2d52b6725467436b7f02
erikleemet <erik.leemet@gmail.com>
2022-12-12 14:08:28 +0200

    new

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,14 @@
 import './Content.css'
 
+
+var data = [
+    {"id": 1, "Project": "mini man", "Eelarve": "10", "Saldo": "100", "Alguskpv": "100", "Lõppkpv": "100"},
+    { "id": 2, "Project": "tall man", "Eelarve": "10", "Saldo": "100", "Alguskpv": "100", "Lõppkpv": "100"},
+   ];
+
+const content = document.getElementsByClassName('table');
+data.forEach(element => content.insertAdjacentHTML('beforebegin', `<tr><td>${element.id}</td><td>${element.Project}</td><td>${element.Eelarve}</td><td>${element.Saldo}</td><td>${element.Alguskpv}</td><td>${element.Lõppkpv}</td></tr>`));
+
 export default function Content() {
     return(
 <div class="table-wrapper">
@@ -15,7 +24,7 @@ export default function Content() {
       <th>Lõpp kpv</th>
     </tr>
   </thead>
-   <tbody id="mainContent">
+   <tbody className="table">
     <tr>
       <td>Anna</td>
       <td>1</td>
